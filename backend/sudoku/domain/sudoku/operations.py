761477fa17347ed9df462df85934892079c2c72e
edwilson <edward.e.j.wilson@gmail.com>
2023-05-21 20:17:59 +0100

    Add a get or create operation for an unattempted sudoku by a player

--- backend/sudoku/domain/sudoku/operations.py
+++ backend/sudoku/domain/sudoku/operations.py
@@ -1,10 +1,27 @@
 # Local application imports
 from data import constants, models
-from domain.sudoku import generate
+from domain.sudoku import generate, queries
 
 
 class UnableToCreateSudoku(generate.SudokuGenerationTimeout):
     pass
+
+
+def get_or_create_unattempted_sudoku_for_player(
+    *, player: models.Player, difficulty: constants.SudokuDifficulty, size: int
+) -> models.Sudoku:
+    """
+    Get a sudoku the player hasn't attempted, otherwise create a new one.
+
+    :raises UnableToCreateSudoku: If sudoku generation timed out.
+    """
+    unattempted_sudokus = queries.get_unattempted_sudokus_for_player(
+        player=player, difficulty=difficulty, size=size
+    )
+    if unattempted_sudoku := unattempted_sudokus.first():
+        return unattempted_sudoku
+
+    return create_new_sudoku(difficulty=difficulty, size=size)
 
 
 def create_new_sudoku(
--- backend/tests/integration/domain/sudoku/test_operations.py
+++ backend/tests/integration/domain/sudoku/test_operations.py
@@ -8,6 +8,38 @@ import pytest
 from data import constants, models
 from domain.sudoku import constants as sudoku_constants
 from domain.sudoku import operations
+from tests import factories
+
+
+@pytest.mark.django_db
+class TestGetOrCreateNewSudokuForPlayer:
+    def test_gets_sudoku_when_there_is_an_unattempted_one_available(self):
+        player = factories.Player()
+        sudoku = factories.Sudoku()
+
+        unattempted_sudoku = operations.get_or_create_unattempted_sudoku_for_player(
+            player=player, difficulty=sudoku.difficulty, size=sudoku.size
+        )
+
+        assert unattempted_sudoku == sudoku
+
+    def test_creates_sudoku_when_there_are_no_unattempted_sudokus_available(self):
+        player = factories.Player()
+
+        # Create a sudoku the player has already attempted
+        sudoku = factories.Sudoku()
+        factories.Game(sudoku=sudoku)
+
+        # Create a sudoku for some other size and difficulty
+        factories.Sudoku(size=9)
+        factories.Sudoku(difficulty=constants.SudokuDifficulty.EASY)
+
+        operations.get_or_create_unattempted_sudoku_for_player(
+            player=player, difficulty=constants.SudokuDifficulty.MEDIUM, size=4
+        )
+
+        # Ensure this was a new sudoku (we made 3 from factories, so a new one would make 4)
+        assert models.Sudoku.objects.count() == 4
 
 
 @pytest.mark.django_db
