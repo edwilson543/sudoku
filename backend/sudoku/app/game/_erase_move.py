f3f3dc3b7552f31726ee586b48f8eb79d92a2b14
edwilson <edward.e.j.wilson@gmail.com>
2023-05-23 21:34:35 +0100

    Update erase move to load the move itself
    - In the case where a move is made and a user wants to delete it, we need to be able to identify the move by row and column rather than id since the frontend may not have received the id yet

--- backend/sudoku/app/game/_erase_move.py
+++ backend/sudoku/app/game/_erase_move.py
@@ -1,8 +1,13 @@
 # Local application imports
 from data import models
+from domain.game import queries
 
 
-def erase_move(*, move: models.Move) -> None:
+class MoveDoesNotExist(Exception):
+    pass
+
+
+def erase_move(*, game: models.Game, row: int, column: int) -> None:
     """
     Erase a single move from the game (but maintain a record that it existed).
 
@@ -10,4 +15,6 @@ def erase_move(*, move: models.Move) -> None:
     * The user realised they made a mistake and wants to retract the remove
     * The user wants to make a new move in the occupied cell
     """
+    if not (move := queries.get_non_erased_move(game=game, row=row, column=column)):
+        raise MoveDoesNotExist
     move.erase()
--- backend/sudoku/app/game/_make_move.py
+++ backend/sudoku/app/game/_make_move.py
@@ -11,11 +11,9 @@ from domain.game import queries as game_queries
 def make_move(*, game: models.Game, row: int, column: int, value: int) -> models.Move:
     """
     Record an attempt at inserting the correct value in a game of sudoku.
-
-    :raises MoveAlreadyExists: If a non-erased move already exists in the given cell.
     """
-    if existing_move := game_queries.get_non_erased_move(game, row=row, column=column):
-        _erase_move.erase_move(move=existing_move)
+    if game_queries.get_non_erased_move(game, row=row, column=column):
+        _erase_move.erase_move(game=game, row=row, column=column)
 
     is_correct = game.sudoku.move_is_correct(row=row, column=column, value=value)
 
