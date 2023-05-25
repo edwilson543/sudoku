7e05bd4ae14dd0fae579b30d3dfb5e61b1942771
edwilson <edward.e.j.wilson@gmail.com>
2023-05-25 19:10:51 +0100

    Update type annotation on request to be a drf request

--- backend/sudoku/interfaces/rest_api/views/_helpers.py
+++ backend/sudoku/interfaces/rest_api/views/_helpers.py
@@ -1,12 +1,12 @@
 # Third party imports
 from rest_framework import exceptions as drf_exceptions
+from rest_framework import request as drf_request
 
 # Django imports
-from django import http
 from django.conf import settings
 
 
-def check_request_source(request: http.HttpRequest) -> None:
+def check_request_source(request: drf_request.Request) -> None:
     """
     Restrict the consumers of the REST API to the frontend.
     """
