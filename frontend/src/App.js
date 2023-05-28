fac9259af774c05d1eb03352ee2a62db9304947c
edwilson <edward.e.j.wilson@gmail.com>
2023-05-28 19:45:06 +0100

    Add a bit more styling

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,10 +10,12 @@ import getOrCreateActiveGame from "./services/rest_api/calls";
 function App() {
   const game = getOrCreateActiveGame();
   return (
-    <div className={"page-content"}>
-        <h1 className={"headstrap"}>Eduko.com</h1>
-        <Game sudoku={game.sudoku} existingMoves={game.moves}/>
-    </div>
+    <>
+        <h1 className={"headstrap"}>Eduko</h1>
+        <div className={"page-content"}>
+            <Game sudoku={game.sudoku} existingMoves={game.moves}/>
+        </div>
+    </>
   );
 }
 
