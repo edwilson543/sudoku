949ac85941ab1d2076c1a911a9896e653d5109aa
edwilson <edward.e.j.wilson@gmail.com>
2023-05-30 21:47:05 +0100

    Fix test in line with name change

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -4,6 +4,6 @@ import App from "./App";
 test("renders app", () => {
   render(<App />);
   // Check header
-  const headStrap = screen.getByText("Eduko");
+  const headStrap = screen.getByText("eduko");
   expect(headStrap).toBeInTheDocument();
 });
