07eb61cf8d13aa99df020a050bdf13b7433f8d17
edwilson <edward.e.j.wilson@gmail.com>
2023-06-02 18:17:44 +0100

    Convert the `ActionPanel` and `ControlPanel` components to use typescript

--- frontend/src/components/controls/ActionPanel.tsx
+++ frontend/src/components/controls/ActionPanel.tsx
@@ -1,13 +1,22 @@
+import React, { SetStateAction } from "react";
+
 import ValidateButton from "./ValidateButton";
 import EraseButton from "./EraseButton";
 import UndoButton from "./UndoButton";
+
+type ActionPanelProps = {
+  activeCell: ActiveCell;
+  validationIsOn: boolean;
+  setValidationIsOn: React.Dispatch<SetStateAction<boolean>>;
+  isSolved: boolean;
+};
 
 export default function ActionPanel({
   activeCell,
   validationIsOn,
   setValidationIsOn,
   isSolved,
-}) {
+}: ActionPanelProps) {
   return (
     <div className={"action-panel"}>
       <UndoButton isSolved={isSolved} />
--- frontend/src/components/controls/ControlPanel.tsx
+++ frontend/src/components/controls/ControlPanel.tsx
@@ -1,6 +1,17 @@
+import React, { SetStateAction } from "react";
+
 import NumberInputPanel from "./NumberInputPanel";
 import ActionPanel from "./ActionPanel";
 import NewGame from "./NewGame";
+
+type ControlPanelProps = {
+  sudoku: Sudoku;
+  activeCell: ActiveCell;
+  setActiveCell: React.Dispatch<SetStateAction<ActiveCell>>;
+  validationIsOn: boolean;
+  setValidationIsOn: React.Dispatch<SetStateAction<boolean>>;
+  isSolved: boolean;
+};
 
 export default function ControlPanel({
   sudoku,
@@ -9,7 +20,7 @@ export default function ControlPanel({
   validationIsOn,
   setValidationIsOn,
   isSolved,
-}) {
+}: ControlPanelProps) {
   return (
     <div className={"control-panel"}>
       <ActionPanel
