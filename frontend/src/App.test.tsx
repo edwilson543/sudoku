1dadfd23a06658cd2071362f85cec1c64c016808
edwilson <edward.e.j.wilson@gmail.com>
2023-06-06 08:42:39 +0100

    Update token (for now) test to run async

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
+import { act } from "react-dom/test-utils";
 
 import App from "./App";
 
 jest.mock("./services/apiClient/RestAPIClient");
 
-test("renders app", () => {
-  render(<App />);
+test("renders app", async () => {
+  await act(() => render(<App />));
   // Check header
   const headStrap = screen.getByText("eduko");
   expect(headStrap).toBeInTheDocument();
