a8bdf188eeccc3ae1cf62563f6007762a7e20c9c
edwilson <edward.e.j.wilson@gmail.com>
2023-10-16 18:55:37 +0100

    Proxy static files for django admin site from nginx server

--- backend/sudoku/config/settings.py
+++ backend/sudoku/config/settings.py
@@ -143,6 +143,7 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = "static"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
